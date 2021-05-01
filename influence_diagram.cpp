34176761ac9e8d49aacdc691621339f4f79be344
fkeidel <f.keidel@gmx.de>
2021-05-02 01:49:59 +0200

    refactoring comments

--- influence_diagram.cpp
+++ influence_diagram.cpp
@@ -38,7 +38,9 @@ namespace Bayes {
 	// Takes an influence diagram and returns a factor over the scope of the 
 	// decision rule d that gives the conditional utility given each assignment for d.var
 	//
-	// Note: assume id has a single decision node and utility node
+	// Note: Assume id has a single decision node and utility node.
+	//       The decision variable must be the first entry in d.var
+	//
 	Factor CalculateExpectedUtilityFactor(const InfluenceDiagram id) 
 	{
 		// In this function, we assume there is only one utility node.
@@ -50,86 +52,74 @@ namespace Bayes {
 		f.push_back(u);
 
 		// get decision variable
-		uint32_t d = id.decision_factors[0].Var(0);
+		const auto d = id.decision_factors[0].Var(0); // only one decision supported
 
-		std::vector<uint32_t> x;
-		for (const auto& factor : id.random_factors)
-		{
-			x.insert(x.end(), factor.Var().begin(), factor.Var().end());
-		}
-		std::sort(x.begin(), x.end());
-		const auto last = std::unique(x.begin(), x.end());
-		x.erase(last, x.end());
+		// x are all variables except decision d
+		std::vector<uint32_t> x{ UniqueVars(id.random_factors) };
 		const auto diff{ Difference(x, std::vector<uint32_t>{d}) };
 		x = diff.values;
 
-		const auto d_var = id.decision_factors[0].Var(); // only one decision
+		// get parents of decision variable
+		const auto d_var = id.decision_factors[0].Var(); 
 		std::vector<uint32_t> d_var_parents(d_var.begin() + 1, d_var.end()); // parents have index 1..
 		
+		// eliminate all random variables except the parents of d
 		const auto x_minus_parents_of_d = Difference(x, d_var_parents);
 		VariableElimination(f, x_minus_parents_of_d.values);
+		// the expected utility factor is the joint distribution of the remaining factors
 		const auto euf = ComputeJointDistribution(f);
 		return euf;
 	}
 
-	// function [MEU OptimalDecisionRule] = OptimizeMEU( I )
+	// (meu, odr) = OptimizeMEU( id )
 	//
-	// Inputs: An influence diagram I with a single decision node and a single utility node.
-	//         I.RandomFactors = list of factors for each random variable.  These are CPDs, with
-	//              the child variable = D.var(1)
-	//         I.DecisionFactors = factor for the decision node.
-	//         I.UtilityFactors = list of factors representing conditional utilities.
-	// Return value: the maximum expected utility of I and an optimal decision rule 
-	// (represented again as a factor) that yields that expected utility.
-	// The order of var in the optimal decison rule is the same as in the internally calculated expected utility factor
-
+	// Takes as input an influence diagram id and returns the maximum expected utility and
+	// an optimal decision rule odr (represented as a factor) that yields that expected utility.
+	// The order of var in the optimal decison rule is the same as in the internally calculated 
+	// expected utility factor euf
+	//
 	OptimizeInfluenceDiagramResult OptimizeMEU(InfluenceDiagram id) 
 	{
-		// We assume I has a single decision node.
-		// You may assume that there is a unique optimal decision.
-		// D = I.DecisionFactors(1);
+		// assume id has a single decision node
+		// assume that there is a unique optimal decision
 		assert(("only one decision factor supported", id.decision_factors.size() == 1));
 		const auto d = id.decision_factors[0];
 
-		// MEU = 0;
 		double meu{ 0.0 };
-		// OptimalDecisionRule = struct('var', [], 'card', [], 'val', []);
 		Factor odr{ d.Var(), d.Card(), std::vector<double>(d.Val().size())}; 
 
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// 1.  It is probably easiest to think of two cases - D has parents and D 
-		//     has no parents.
-		// 2.  You may find the Matlab/Octave function setdiff useful.
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 		const auto euf = CalculateExpectedUtilityFactor(id);
-		if (d.Var().size() == 1) { // decision variable has no parents
+
+		if (d.Var().size() == 1) 
+		{ 
+			// decision variable has no parents
 			odr = euf;  // copy var and card from euf
 			// calc values
-			uint32_t n_val{ std::accumulate(d.Card().begin(), d.Card().end(), 1U, std::multiplies<uint32_t>()) };
-			std::vector<double> val(n_val, 0.0);
+			std::vector<double> val(d.Val().size(), 0.0); 
 			// all 0 except the entry that corresponds to the max value of the expected utility factor
 			auto it = std::max_element(euf.Val().begin(), euf.Val().end());
 			meu = *it;
-			val[it - euf.Val().begin()] = 1.0; // best decisision
+			val[it - euf.Val().begin()] = 1.0; // mark best decisision
 			odr.SetVal(val);
 			return { meu, odr };
 		}
 
 		// decision variable has parents
 
-		//  DIndexInEUF = find(EUF.var == D);
+		// find index of decision variable in expected utility factor
 		const auto& it = std::find(euf.Var().begin(), euf.Var().end(), d.Var(0)); // expect decision variable as first entry in d
 		assert(("d must be in EUF", it != euf.Var().end()));
 		const auto d_index_in_euf = std::distance(euf.Var().begin(), it);
 
-		//  D.card = EUF.card( dIndexInEUF );
+		// get cardinality of decision variable
 		const auto d_card = euf.Card(d_index_in_euf);
 
+		// get parents of decision variable
 		const auto parents_diff_result = Difference(euf.Var(), { d.Var(0) });
 		const auto parents_var{ parents_diff_result.values };
 		const auto mapP2Euf{ parents_diff_result.left_indices };
 
-		// Parents is a dummy factor to create parent assignments
+		// create a dummy factor for parents to create parent assignments
 		std::vector<uint32_t> parents_card(parents_var.size(), 0);
 		for (size_t i = 0; i < parents_var.size(); ++i) {
 			parents_card[i] = euf.Card(mapP2Euf[i]);
@@ -145,32 +135,30 @@ namespace Bayes {
 		const auto mapD = mapping.right_indices;
 
 		// For each assignment of parents ... determine best decision
-		// for i=1:length(Parents.val)
 		for (size_t i = 0; i < parents_val.size(); ++i) {
 			const auto a_pa = parents.IndexToAssignment(i);
 			std::vector<uint32_t> a_euf(euf.Var().size(), 0);
 			for (size_t k = 0; k < parents_var.size(); ++k) {
 				a_euf[mapP2Euf[k]] = a_pa[k];
 			}
-			// test each decision
+			// test each decision d
 			double max_value{ 0.0 };
 			size_t idx_euf_max_value{ 0 };
-			// for j=1:d.card
-			for (uint32_t j = 0; j < d_card; ++j) {
-				a_euf[d_index_in_euf] = j;
+			for (uint32_t d = 0; d < d_card; ++d) {
+				a_euf[d_index_in_euf] = d;
 				const auto idx = euf.AssigmentToIndex(a_euf);
 				const auto value = euf.Val(idx);
 				if (value > max_value) {
 					max_value = value;
 					idx_euf_max_value = idx;
 				}
-			} // end
+			}
 			// set corresponding entry in decison rule
 			const auto a_euf_max = euf.IndexToAssignment(idx_euf_max_value);
 			std::vector<uint32_t> a_d(d.Var().size(), 0U);
-			for (size_t j = 0; j < d.Var().size(); ++j) {
+			for (size_t v = 0; v < d.Var().size(); ++v) {
 				// a_euf -> a_d
-				a_d[mapD[mapEuf[j]]] = a_euf_max[j];
+				a_d[mapD[mapEuf[v]]] = a_euf_max[v];
 			}
 			const auto idx_odr = odr.AssigmentToIndex(a_d);
 			odr.SetVal(idx_odr, 1);
--- tests/influence_diagram_test.cpp
+++ tests/influence_diagram_test.cpp
@@ -43,7 +43,7 @@ namespace Bayes {
 		ExpectVectorElementsNear(all_eus, expected);
 
 		// 2. Calculate expected utility with expected utility factor
-		Factor d{ {1}, {2}, {} }; // empty decision rule 
+		Factor d{ {1}, {2}, {0,0} }; // dummy decision rule 
 		InfluenceDiagram id{ {x},{d},{u} };
 		const auto euf = CalculateExpectedUtilityFactor(id);
 		Factor euf_expected{ {1}, {2}, expected };
@@ -56,15 +56,16 @@ namespace Bayes {
 	}
 
 	TEST(InfluenceDiagram, WhenX0IsKnownToD) {
-		// Uhe decision factor d for decision variable 1 has now a parent 
+		// The decision factor d for decision variable 1 has now a parent 
 		// factor x0 for random variable 0. The utility factor u is now a function
 		// of the decision d and the factor x2 for random variable 2.
+		// x2 has parents 0 and 1
 		// 
-		//           x0(0)
-		//             \
-		//              \
-		//              `´
-		//  x2(2)       d(1,0)
+		//       x0(0)
+		//      /     \
+		//     /       \
+		//    `´       `´
+		//  x2(2) <--- d(1,0)
 		//    \        /
 		//     \      /
 		//     `´    `´
@@ -72,82 +73,39 @@ namespace Bayes {
 
 		Factor x0{ {0}, {2}, {7, 3} };
 		x0.Normalize();
-	
-		Factor d{ {1,0}, {2,2}, {1,0,0,1} };
 		Factor x2{ {2,0,1}, {2,2,2}, {4, 4, 1, 1, 1, 1, 4, 4} };
 		x2 = x2.CPD(2);
 
+		Factor d0{ {1,0}, {2,2}, {1,0,1,0} };
+		Factor d1{ {1,0}, {2,2}, {1,0,0,1} };
+		Factor d2{ {1,0}, {2,2}, {0,1,1,0} };
+		Factor d3{ {1,0}, {2,2}, {0,1,0,1} };
+		// vector with all possible decision rules
+		std::vector<Factor> all_ds{ d0, d1, d2, d3 };
+
 		Factor u{ {1,2}, {2,2}, {10, 1, 5, 1} };
 
-		//I3.RandomFactors = [X1 X3];
-		//I3.DecisionFactors = D;
-		//I3.UtilityFactors = U1;
-		InfluenceDiagram i3{ {x0,x2},{d}, {u} };
-
-		// All possible decision rules
-		//D1 = D; D2 = D; D3 = D; D4 = D;
-		auto d1_0(d);
-		auto d1_1(d);
-		auto d1_2(d);
-		auto d1_3(d);
-		//D1.val = [1 0 1 0];
-		//D2.val = [1 0 0 1];
-		//D3.val = [0 1 1 0];
-		//D4.val = [0 1 0 1];
-		d1_0.SetVal({ 1,0,1,0 });
-		d1_1.SetVal({ 1,0,0,1 });
-		d1_2.SetVal({ 0,1,1,0 });
-		d1_3.SetVal({ 0,1,0,1 });
-
-		//AllDs = [D1 D2 D3 D4];
-		std::vector<Factor> all_ds{ d1_0, d1_1, d1_2, d1_3 };
-
-		//allEU = zeros(length(AllDs), 1);
+		// 1. Naive approach: calculate expected utilities for each decision rule
 		std::vector<double> all_eus(all_ds.size());
-		//for i = 1:length(AllDs)
 		for (size_t i = 0; i < all_ds.size(); ++i) {
-			//I3.DecisionFactors = AllDs(i);
-			i3.decision_factors = { all_ds[i] };
-			//allEU(i) = SimpleCalcExpectedUtility(I3);
-			all_eus[i] = SimpleCalcExpectedUtility(i3);
-		} //end
+			const auto& decision_rule{ all_ds[i] };
+			InfluenceDiagram id{ {x0,x2},{decision_rule}, {u} };
+			all_eus[i] = SimpleCalcExpectedUtility(id);
+		} 
+		std::vector<double> expected{ 7.50, 5.55, 2.95, 1.00 };
+		ExpectVectorElementsNear(all_eus, expected);		
+	
+		// 2. Calculate expected utility with expected utility factor
+		Factor d{ {1,0}, {2,2}, {0,0,0,0} }; // dummy decision rule
+		InfluenceDiagram id{ {x0,x2},{d}, {u} };
 
-
-		// Get EUF...
-		// euf = CalculateExpectedUtilityFactor(I3);
-		const auto euf = CalculateExpectedUtilityFactor(i3);
+		const auto euf = CalculateExpectedUtilityFactor(id);
 		Factor euf_expected{ {0,1}, {2,2}, {5.25,2.25,0.7,0.3} };
 		ExpectFactorEqual(euf, euf_expected);
 
-		// PrintFactor(euf) =>
-		// 1	2	
-		// 1	1	5.250000
-		// 2	1	2.250000
-		// 1	2	0.700000
-		// 2	2	0.300000
-
-		// [meu optdr] = OptimizeMEU(I3)
-		const auto result = OptimizeMEU(i3);
+		const auto result = OptimizeMEU(id);
 		EXPECT_NEAR(result.meu, 7.5, 0.001);
 		Factor odr_expected{ {1,0}, {2,2}, { 1,0,1,0} };
 		ExpectFactorEqual(result.odr, odr_expected);
-
-		// [meu optdr] = OptimizeWithJointUtility(I3)
-		// [meu optdr] = OptimizeLinearExpectations(I3)
-
-		// OUTPUT
-		// allEU =
-		// 7.5000
-		// 5.5500
-		// 2.9500
-		// 1.0000
-		// 
-		// meu = 7.5000
-		// PrintFactor(optdr) => 
-		// 2	1	
-		// 1	1	1.000000
-		// 2	1	0.000000
-		// 1	2	1.000000
-		// 2	2	0.000000
 	}
 }
