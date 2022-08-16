c6c2984571529ddb452497307ceef80d91dd6a1e
Frank Keidel <frank.keidel@bmw.de>
2022-08-16 17:01:42 +0200

    Read table from csv

--- examples/example_utils.h
+++ examples/example_utils.h
@@ -37,4 +37,46 @@ void WriteTableToCsv(const std::string file_path, const std::vector<std::vector<
 	file.close();
 }
 
+template <class T>
+std::vector<std::vector<T>> ReadTableFromCsv(const std::string file_path, bool skip_first_row = false)
+{
+    std::ifstream file(file_path);
+    std::vector<std::vector<T>> result;
+    if (file)
+    {
+        std::string line;
+        if (skip_first_row)
+        {
+            std::getline(file, line);
+        }
+        while (std::getline(file, line))
+        {
+            std::stringstream ss(line);
+            std::vector<T> row;
+            T val;
+            while (ss >> val)
+            {
+                row.push_back(val);
+                if (ss.peek() == ',')
+                    ss.ignore();
+            }
+            result.push_back(row);
+        }
+    }
+    else
+        std::cout << "Cannot read file: " << file_path << std::endl;
+    return result;
+}
+
+template <class T>
+std::vector<T> GetColumn(const std::vector<std::vector<T>>& data, size_t column)
+{
+    std::vector<T> result(data.size(), 0.0);
+    for (size_t row = 0; row < data.size(); ++row)
+    {
+        result[row] = data[row][column];
+    }
+    return result;
+}
+
 #endif 
